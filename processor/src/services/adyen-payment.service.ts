a3e07021f1e84e28705c008abb967d6e4554e875
dasanor <david.sanchez@commercetools.com>
2024-05-06 11:10:02 +0200

    fix(payments): solved issue when creating a payment from a cart that contains both a customer id and an anonymous id. SCC-2292

--- processor/src/services/adyen-payment.service.ts
+++ processor/src/services/adyen-payment.service.ts
@@ -241,9 +241,10 @@ export class AdyenPaymentService extends AbstractPaymentService {
             id: ctCart.customerId,
           },
         }),
-        ...(ctCart.anonymousId && {
-          anonymousId: ctCart.anonymousId,
-        }),
+        ...(!ctCart.customerId &&
+          ctCart.anonymousId && {
+            anonymousId: ctCart.anonymousId,
+          }),
       });
 
       ctCart = await this.ctCartService.addPayment({
