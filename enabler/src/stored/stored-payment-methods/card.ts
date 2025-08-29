f6ddf58cba4f8e409163c192b26847c8232c5ec8
Joey Koster <joey.koster@commercetools.com>
2025-08-29 09:11:17 +0200

    chore(enabler): update todo comment ticket number

--- enabler/src/stored/stored-payment-methods/card.ts
+++ enabler/src/stored/stored-payment-methods/card.ts
@@ -78,7 +78,7 @@ export class StoredCardComponent extends DefaultAdyenStoredComponent {
   }
 
   async remove() {
-    // TODO: SCC-3447: make HTTP DELETE call to remove it. See the drop-in component and the "onDisableStoredPaymentMethod" function.
+    // TODO: SCC-3471: make HTTP DELETE call to remove it. See the drop-in component and the "onDisableStoredPaymentMethod" function.
     console.log("TODO: Implement remove method for stored card component");
     return;
   }
