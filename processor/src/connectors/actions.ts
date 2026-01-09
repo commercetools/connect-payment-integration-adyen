54dca9201ff1107046cc74c00fb2426bbb35065f
Joey Koster <joey.koster@commercetools.com>
2026-01-09 16:07:20 +0100

    chore(processor): update log message

--- processor/src/connectors/actions.ts
+++ processor/src/connectors/actions.ts
@@ -4,7 +4,7 @@ import { paymentSDK } from '../payment-sdk';
 
 export async function createCheckoutCustomType(): Promise<void> {
   if (!getConfig().adyenStorePaymentMethodDetailsEnabled) {
-    log.info('Not creating the custom types for Checkout since the feature is disabled');
+    log.info('Not creating the predefined payment method custom-types for Checkout since the feature is disabled');
     return;
   }
 
